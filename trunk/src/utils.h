7816128f8ae8d931a4e6aa7d1e83df5e01bbb4ca
ggpolo <ggpolo@6792eda6-be33-11dd-ad2b-77eeadef73a9>
2008-11-29 17:44:08 +0000

    Forgot some dead code here

--- trunk/src/utils.h
+++ trunk/src/utils.h
@@ -22,30 +22,6 @@
         }                                                   \
     } while (0)
 
-#if 0
-typedef struct {
-	PyObject_HEAD
-	/* type specific fields follow */
-	Tcl_Interp *interp;
-
-	Tcl_ThreadId tcl_thread_id;
-	long py_thread_id;
-
-	int running;
-	int tk_loaded;
-	int _error_in_cb; /* 1 when error occurs in a callback, 2 when bgerror */
-
-	PyObject *bgerr_handler;
-	PyObject *commands;
-	/* Tcl types */
-	Tcl_ObjType *IntType;
-	Tcl_ObjType *ListType;
-	Tcl_ObjType *DictType;
-	Tcl_ObjType *DoubleType;
-	Tcl_ObjType *ByteArrayType;
-} TclInterpObj;
-#endif
-
 PyObject *TclObj_ToPy(TclInterpObj *, Tcl_Obj *);
 Tcl_Obj *PyObj_ToTcl(PyObject *);
 
