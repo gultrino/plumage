04da4e07073bad522273d645fc1cbd93ab7266a5
ggpolo <ggpolo@6792eda6-be33-11dd-ad2b-77eeadef73a9>
2008-12-12 17:55:12 +0000

    When did I move test into src/ ?

---
+++ trunk/test/test_tclnull_tonull.py
@@ -0,0 +1,22 @@
+import support
+import unittest
+
+import _tclnull_tonull
+
+tests = (
+        ("\xC0\x80", "\x00"),
+        ("\xC0\x80\xC0\x80", "\x00\x00"),
+        ("\xC0\200a\xC0\x80", "\000a\x00"),
+        ("hi\xC0\x80ihabc", "hi\x00ihabc")
+        )
+
+class ConversionTest(unittest.TestCase):
+    def test_conversion(self):
+        for test in tests:
+            self.failUnlessEqual(_tclnull_tonull.test_conversion(*test), None)
+
+def test_main():
+    support.run(ConversionTest)
+
+if __name__ == "__main__":
+    test_main()
