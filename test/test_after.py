a8c93c15eaac6682b76b4a70092549e6de7d141d
ggpolo <ggpolo@6792eda6-be33-11dd-ad2b-77eeadef73a9>
2008-12-17 16:43:25 +0000

    Added test for checking memory leak in Misc.after

---
+++ test/test_after.py
@@ -0,0 +1,33 @@
+import unittest
+import threading
+
+import support
+import Tkinter
+
+# XXX this may move to another module
+class AfterCmdTest(unittest.TestCase):
+
+    def setUp(self):
+        self.root = Tkinter.Tk()
+
+    def tearDown(self):
+        self.root.destroy()
+
+    def test_after_leak(self):
+        cmd_len = len(self.root._tclCommands)
+
+        try:
+            self.root.after("bad", lambda: None)
+        except Tkinter.TclError:
+            # nothing should have been added to cmds
+            pass
+
+        self.failUnlessEqual(cmd_len, len(self.root._tclCommands))
+
+
+
+def test_main():
+    support.run(AfterCmdTest)
+
+if __name__ == "__main__":
+    test_main()
